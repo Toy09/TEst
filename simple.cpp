853c4ee550480fc3c2e30ba4313274cdbee4544b
jongwoo <jongwoo@codemind.co.kr>
2021-06-24 09:46:05 +0900

    ...

--- simple.cpp
+++ simple.cpp
@@ -5,7 +5,7 @@ void test(int x) {
   int y = 0;
   bool tmp = false;
   do {
-    tmp = (y == 0) || (y % 10 == 0);
+    tmp = (y == 0) && (y % 10 == 0);
     try {
       printf("y[%d]:%d\n", tmp, (y > 10) ? (y % 10) : y);
     } catch (std::runtime_error &e) {
