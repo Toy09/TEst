01194fc480363ccffa2637943ad58c6cae1dbc73
jongwoo <jongwoo@codemind.co.kr>
2021-06-24 10:37:20 +0900

    ...

--- simple.cpp
+++ simple.cpp
@@ -2,26 +2,5 @@
 #include <iostream>
 
 void test(int x) {
-  int y = 0;
-  bool tmp = false;
-  do {
-    switch(y) {
-      case 50:
-        tmp = false;
-        break;
-      default :
-        tmp = (y == 0) && (y % 10 == 0);
-    }
-    ?
-
-    try {
-      printf("y[%d]:%d\n", tmp, (y > 10) ? (y % 10) : y);
-      goto NAME;
-    } catch (std::runtime_error &e) {
-      break;
-    }
-    y++;
-  } while((y < x) ^ (y <= 100));
-  NAME:
-    return;
+  ?
 }
